5182375bde2780603a5e406400cfb61e5d6e8490
Vidya Tiwari <vidya.tiwari@novagito.com>
2025-05-14 21:03:46 +0530

    redux practice

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AddTodo from './components/AddTodo.jsx'
+import Todos from './components/Todo.jsx'
+
+function App() {
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
