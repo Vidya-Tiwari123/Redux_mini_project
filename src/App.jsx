0876b3a638a6cdcd2fa6bf0fc390d2d608e14ec7
Vidya Tiwari <vidya.tiwari@novagito.com>
2025-05-15 17:02:22 +0530

    fixed button style

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Todos from './components/Todo.jsx'
 function App() {
   return (
     <>
-      <h1>Learn about redux toolkit</h1>
+      <h1 className='flex justify-center text-4xl font-bold p-4'>Todo App</h1>
       <AddTodo />
       <Todos />
     </>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import deleteicon from "../assets/icons8-delete-30.svg";
 import { removeTodo } from "../features/todo/todoSlice";
 
 
@@ -10,17 +10,16 @@ function Todos(){
 
     return(
         <>
-        <div>Todos</div>
-     <ul className="list-none">
+     <ul className="list-none p-5">
      {todos.map((todo)=>(
             <li 
-            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+            className="mt-8 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded-lg"
             key={todo.id}>
                 
                <div className="text-white"> {todo.text}</div>
             <button onClick={()=>dispatch(removeTodo(todo.id))}
                 className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none"
-                >*</button>
+                > <img src={deleteicon} alt="Delete" className="w-5 h-5" /></button>
             
             </li>
         ))}
